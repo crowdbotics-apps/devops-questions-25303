5acb4fadfd7c480f79532a8209d348b07eb3543a
Crowdbotics <team@crowdbotics.com>
2021-03-27 06:52:00 +0000

    Initial commit.

---
+++ ios/devops_questions_25303Tests/devops_questions_25303Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface devops_questions_25303 : XCTestCase
+
+@end
+
+@implementation devops_questions_25303
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://devops-questions-25303.botics.co",
+  defaultTimeout: 5000
+};
